dee4e85a99fd015805d8a7b61933f2a36f861f22
Bud Sherwood <sherwoodiv@gmail.com>
2025-11-26 22:29:44 -0500

    added repo name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+app: {
+    baseURL: '/clay_score_card/',   // Important!
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
