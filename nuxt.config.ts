28a53412ba81b83c831a3c79f9a99f6cda78398a
Bud Sherwood <sherwoodiv@gmail.com>
2025-11-26 22:53:54 -0500

    added repo name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,6 @@ export default defineNuxtConfig({
     baseURL: '/clay_score_card/',   // Important!
     buildAssetsDir: '_nuxt/'
   },
-
+  css: ['@nuxt/ui/dist/ui.css'],
   modules: ['@nuxt/ui'],
 })
