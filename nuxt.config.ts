acc32d498d600049f4430a91a5206ae30dbb671d
Bud Sherwood <sherwoodiv@gmail.com>
2025-11-26 22:34:50 -0500

    added repo name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-app: {
+  ssr: false,
+  nitro: { preset: 'static' },
+  app: {
     baseURL: '/clay_score_card/',   // Important!
   },
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/ui'
-  ],
-  nitro: {
-    preset: 'static',             // generates a fully static site in .output/public
-    output: { publicDir: 'dist' } // optional – makes the folder name nicer
-  }
 })
