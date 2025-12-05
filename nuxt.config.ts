4108353b27f1a328d32ac9a2cdc4c688c6cd0408
Bud Sherwood <sherwoodiv@gmail.com>
2025-12-04 21:22:41 -0500

    fixed styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,47 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-        '@nuxt/eslint',
-        '@nuxt/ui'
-    ],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui'
+  ],
 
-    devtools: {
-        enabled: true
-    },
+  devtools: {
+    enabled: true
+  },
+  app: {
+    baseURL: '/clay_score_card/' // ← THIS IS THE MAGIC LINE
+    // buildAssetsDir: '_nuxt'     // default, you can leave it
+  },
 
-    css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/main.css'],
 
-    routeRules: {
-        '/': {prerender: true}
-    },
-
-    compatibilityDate: '2025-01-15',
-
-    eslint: {
-        config: {
-            stylistic: {
-                commaDangle: 'never',
-                braceStyle: '1tbs'
-            }
-        }
-    },
-    app: {
-        baseURL: '/clay_score_card/'   // ← THIS IS THE MAGIC LINE
-        // buildAssetsDir: '_nuxt'     // default, you can leave it
-    },
-
-    // Also very important for GitHub Pages
-    nitro: {
-        baseURL: '/clay_score_card/',   // newer Nitro versions need this too
-        prerender: {
-            routes: ['/']   // makes sure index.html is generated at root
-        }
-    },
-
-    // If you're using <NuxtLink> or router, this helps too
-    router: {
-        options: {
-            linkActiveClass: 'active'
-        }
+  // If you're using <NuxtLink> or router, this helps too
+  router: {
+    options: {
+      linkActiveClass: 'active'
     }
+  },
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  // Also very important for GitHub Pages
+  nitro: {
+    baseURL: '/clay_score_card/', // newer Nitro versions need this too
+    prerender: {
+      routes: ['/'] // makes sure index.html is generated at root
+    }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
 })
