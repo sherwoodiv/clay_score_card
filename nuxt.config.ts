4095cbca94cad0d34dac4083543b31f603bcc459
Bud Sherwood <sherwoodiv@gmail.com>
2025-12-04 21:05:43 -0500

    Added ghpages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui'
-  ],
+    modules: [
+        '@nuxt/eslint',
+        '@nuxt/ui'
+    ],
 
-  devtools: {
-    enabled: true
-  },
+    devtools: {
+        enabled: true
+    },
 
-  css: ['~/assets/css/main.css'],
+    css: ['~/assets/css/main.css'],
 
-  routeRules: {
-    '/': { prerender: true }
-  },
+    routeRules: {
+        '/': {prerender: true}
+    },
 
-  compatibilityDate: '2025-01-15',
+    compatibilityDate: '2025-01-15',
 
-  eslint: {
-    config: {
-      stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
+    eslint: {
+        config: {
+            stylistic: {
+                commaDangle: 'never',
+                braceStyle: '1tbs'
+            }
+        }
+    },
+    app: {
+        baseURL: '/clay_score_card/'   // ← THIS IS THE MAGIC LINE
+        // buildAssetsDir: '_nuxt'     // default, you can leave it
+    },
+
+    // Also very important for GitHub Pages
+    nitro: {
+        baseURL: '/clay_score_card/',   // newer Nitro versions need this too
+        prerender: {
+            routes: ['/']   // makes sure index.html is generated at root
+        }
+    },
+
+    // If you're using <NuxtLink> or router, this helps too
+    router: {
+        options: {
+            linkActiveClass: 'active'
+        }
     }
-  }
 })
